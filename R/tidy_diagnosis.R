9537d4a25863b3bf2c83402facb084d400bfa397
Graeme Blair <graemeblair@users.noreply.github.com>
2021-05-30 19:40:58 -0400

    tidy

--- R/tidy_diagnosis.R
+++ R/tidy_diagnosis.R
@@ -17,7 +17,7 @@ tidy_diagnosis <- function(diagnosis) {
     mutate(is_se = str_detect(diagnosand, "se\\("),
            diagnosand = str_remove(diagnosand, "se\\("),
            diagnosand = str_remove(diagnosand, "\\)")) %>%
-    pivot_wider(id_cols = c(design, n_x1, inquiry, estimator, term, diagnosand), names_from = is_se, values_from = value) %>%
+    pivot_wider(id_cols = c(diagnosis$group_by_set, "diagnosand"), names_from = is_se, values_from = value) %>%
     rename(estimate = `FALSE`, std.error = `TRUE`) %>%
     mutate(conf.low = estimate - 1.96 * std.error,
            conf.high = estimate + 1.96 * std.error)

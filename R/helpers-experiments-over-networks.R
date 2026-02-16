72f40ef16debb83ef552fd358cd5db968bb3ea58
Macartan Humphreys <macartan@gmail.com>
2026-02-16 13:09:41 +0100

    restore estimator_AS

--- R/helpers-experiments-over-networks.R
+++ R/helpers-experiments-over-networks.R
@@ -26,6 +26,7 @@ get_exposure_AS <- function(obs_exposure) {
 #'
 #' Runs estimates estimation function from interference package and returns tidy data frame output
 #'
+#'
 #' The estimator_AS_tidy function requires the 'interference' package, which is not yet available on CRAN.
 #'
 #' To use this function:
@@ -34,9 +35,30 @@ get_exposure_AS <- function(obs_exposure) {
 #'
 #' See https://book.declaredesign.org/experimental-causal.html#experiments-over-networks
 #'
-#' @param data a data.frame
-#' @param permutatation_matrix a permutation matrix of random assignments
-#' @param adj_matrix an adjacency matrix
+#' @param data A `data.frame` containing the observed data.
+#'
+#' @param permutation_matrix A matrix of random treatment assignments. Each row
+#'   corresponds to one permutation of the treatment vector.
+#'
+#' @param adj_matrix An adjacency matrix defining the network structure. This
+#'   can be created, for example, as follows:
+#'   \preformatted{
+#'   adjacency <- fairfax |>
+#'     as("Spatial") |>
+#'     spdep::poly2nb(queen = TRUE) |>
+#'     spdep::nb2mat(style = "B", zero.policy = TRUE)
+#'   }
+#'
+#' @param obs_prob_exposure A set of exposure probabilities. These can be
+#'   generated, for example, using:
+#'   \preformatted{
+#'   prob_exposure <- interference::make_exposure_prob(
+#'     potential_tr_vector = permutations,
+#'     adj_matrix = adjacency,
+#'     exposure_map_fn = interference::make_exposure_map_AS,
+#'     exposure_map_fn_add_args = list(hop = 1)
+#'   )
+#'   }
 #'
 #' @return a data.frame of estimates
 #'
@@ -45,7 +67,28 @@ get_exposure_AS <- function(obs_exposure) {
 #' @importFrom tibble tibble
 #'
 estimator_AS_tidy <-
-  function(data, permutatation_matrix, adj_matrix) {
-    message("The estimator_AS_tidy function requires the 'interference' package, which is not yet available on CRAN. To use this function, install the developer version of interference via remotes::install_github('szonszein/interference') and then install the developer version of rdss via remotes::install_github('DeclareDesign/rdss@remotes')")
-    return(invisible())
+  function(data, p_matrix, adj_matrix, obs_prob_exposure) {
+
+    obs_exposure <-
+      interference::make_exposure_map_AS(
+        adj_matrix = adj_matrix,
+        tr_vector = data$Z,
+        hop = 1
+      )
+
+    out_AS <-
+      interference::estimates(
+        obs_exposure = obs_exposure,
+        obs_outcome = data$Y,
+        obs_prob_exposure = obs_prob_exposure,
+        n_var_permutations = 30
+      )
+
+    tibble(
+      term = c(names(out_AS$tau_ht), names(out_AS$tau_h)),
+      inquiry = rep(c("total_ATE", "direct_ATE", "indirect_ATE"), 2),
+      estimator = rep(c("Horvitz-Thompson", "Hajek"), each = 3),
+      estimate = c(out_AS$tau_ht, out_AS$tau_h)
+    )
   }
+
